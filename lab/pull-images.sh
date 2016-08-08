bc2026f37c35e88a1927bffdd3f224c0de1dec4f
Rafael Benevides <rafabene@gmail.com>
2016-08-08 12:31:25 -0400

    Minor review

---
+++ lab/pull-images.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker pull rafabene/microservices-frontend:1.0
+docker pull rafabene/microservices-helloworld-vertx:1.0
+docker pull rafabene/microservices-guestbook:1.0
+docker pull openshift3/nodejs-010-rhel7
+docker pull openshift/mysql-56-centos7
+docker pull openshift/nodejs-010-centos7
